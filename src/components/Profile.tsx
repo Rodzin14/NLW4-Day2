f6f3331fc3082561d372dcd7e505183ef28bfcc4
Bruno Rodrigues <bgrpelegrina@gmail.com>
2021-02-26 17:46:32 -0300

    Toda dia 4 (Funcionamentos e notificações)

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext)
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/Rodzin14.png" alt="Bruno Rodrigues" />
@@ -8,7 +13,7 @@ export function Profile() {
                 <strong>Bruno Rodrigues</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
